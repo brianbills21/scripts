af23ce4d516465ba699201cfa4dc6f03599fb879
Brian Bills <15617031+brianbills21@users.noreply.github.com>
2019-07-05 15:50:37 -0700

    Update ubuntu-preseed-post-install-script.sh

--- ubuntu-preseed-post-install-script.sh
+++ ubuntu-preseed-post-install-script.sh
@@ -6,27 +6,35 @@
 # sudo wget http://archive.ubuntu.com/ubuntu/dists/xenial-updates/main/installer-amd64/current/images/netboot \ #
 # /ubuntu-installer/amd64/initrd.gz                                                                             #
 # sudo wget http://archive.ubuntu.com/ubuntu/dists/xenial-updates/main/installer-amd64/current/images/netboot \ #
-# @ /ubuntu-installer/amd64/linux                                                                               #
+# /ubuntu-installer/amd64/linux                                                                               #
 #                                                                                                               #
 # or the install will hang.                                                                                     #
 #################################################################################################################
 
 #!/bin/bash
 
+# STES UP THE ENVIRONMENT
 /usr/bin/apt-get install -y curl && /usr/bin/apt-get install -y openssh-server && /usr/bin/apt-get install -y wget
 
+# GRABS THE PUPPET CLIENT
 curl -O https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
 
+# INSTALLS THE PUPPET CLIENT
 dpkg -i puppetlabs-release-pc1-xenial.deb
 
+# RUNS AN UPDATE
 apt-get update
 
+# INSTALLS THE PUPPET AGENT ON THE NEW HOST
 /usr/bin/apt-get install -y puppet-agent
 
+# UPGRADES UBUNTU 16.04 LTS TO 18.04 LATEST BETA
 /usr/bin/apt update && /usr/bin/apt -y upgrade && /usr/bin/apt install ubuntu-release-upgrader-core && /usr/bin/apt -y update && do-release-upgrade -d -f DistUpgradeViewNonInteractive
 
+# INSTALLS AND CONFIRGURES NTP
 /usr/bin/apt install -y ntp ntpdate && /usr/bin/ntpdate -u 0.ubuntu.pool.ntp.org && /usr/bin/timedatectl set-timezone America/Los_Angeles
 
+# COPIES KEYS FOR ROOT AND USER STORED ON YOUR PRESEED SERVER TO THE NEW HOST, MAKES ALLOWANCE FOR ROOT TO REMOTE LOGIN
 /bin/mkdir -p /root/.ssh
 /bin/mkdir -p /home/bbills/.ssh
 /usr/bin/curl -o /root/.ssh/authorized_keys http://192.168.134.3/bbills/ssh-key/authorized_keys
@@ -34,6 +42,7 @@ apt-get update
 /bin/chown -R bbills:bbills /home/bbills
 /bin/sed -i 's/PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
 
+# CONFIGURES NEW PUPPET AGENT
 cat > /etc/puppetlabs/puppet/puppet.conf <<EOF
 [main]
 certname = puppet-agent.mj12net.local
@@ -50,4 +59,5 @@ sed -i "s/certname = puppet-agent/certname = "$host_name"/g" /etc/puppetlabs/pup
 
 sudo /opt/puppetlabs/bin/puppet agent --test
 
+# INSTALLS VMWARE TOOLS ON NEW VMWARE VIRTUAL MACHINE
 /usr/bin/apt-get install -y open-vm-tools
