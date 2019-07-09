cffcf9857d883a31db96bda5f2442a0a4534c567
Brian Bills <15617031+brianbills21@users.noreply.github.com>
2019-07-09 09:48:23 -0700

    Update monitor-webwasher-proxy-logs.sh

--- monitor-webwasher-proxy-logs.sh
+++ monitor-webwasher-proxy-logs.sh
@@ -9,14 +9,14 @@ to determine which hosts have yet to be migrated from McAfee to Cylance.
 TODAY=`date +"%y%m%d"`
 set -x
 if [ ! -h logs ]; then
-ln -s /opt/webwasher-csm/logs/ logs
+    ln -s /opt/webwasher-csm/logs/ logs
 fi
 grep -E "(login|data-vs2|update|api|api2|my-vs2|download)\.cylance\.com" logs/access$TODAY* | grep -Eo "(10|172)(\.[0-9]{1,3}){3}" | sort -u >> dailyclientip
 echo `date +%y%m%d-%H:%M:%S` >> dailyclients.`hostname`.$TODAY.csv
 for i in `cat dailyclientip`
 do
-echo $i",""$(nslookup $i | sed -n -e 's/^.*= //p' | sed 's/.$//')" >> dailyclients.`hostname`.$TODAY.csv
-sed -i -e '/^,/d' dailyclients.`hostname`.$TODAY.csv
+    echo $i",""$(nslookup $i | sed -n -e 's/^.*= //p' | sed 's/.$//')" >> dailyclients.`hostname`.$TODAY.csv
+    sed -i -e '/^,/d' dailyclients.`hostname`.$TODAY.csv
 done
 scp dailyclients.`hostname`.$TODAY.csv root@modpxy008:~/
 rm dailyclientip
@@ -26,14 +26,14 @@ rm dailyclientip
 TODAY=`date +"%y%m%d"`
 set -x
 if [ ! -h logs ]; then
-ln -s /opt/webwasher-csm/logs/ logs
+    ln -s /opt/webwasher-csm/logs/ logs
 fi
 grep -E "(login|data-vs2|update|api|api2|my-vs2|download)\.cylance\.com" logs/access$TODAY* | grep -Eo "(10|172)(\.[0-9]{1,3}){3}" | sort -u >> dailyclientip
 echo `date +%y%m%d-%H:%M:%S` >> dailyclients.`hostname`.$TODAY.csv
 for i in `cat dailyclientip`
 do
-echo $i",""$(nslookup $i | sed -n -e 's/^.*= //p' | sed 's/.$//')" >> dailyclients.`hostname`.$TODAY.csv
-sed -i -e '/^,/d' dailyclients.`hostname`.$TODAY.csv
+    echo $i",""$(nslookup $i | sed -n -e 's/^.*= //p' | sed 's/.$//')" >> dailyclients.`hostname`.$TODAY.csv
+    sed -i -e '/^,/d' dailyclients.`hostname`.$TODAY.csv
 done
 scp dailyclients.`hostname`.$TODAY.csv root@modpxy007:~/
 rm dailyclientip
