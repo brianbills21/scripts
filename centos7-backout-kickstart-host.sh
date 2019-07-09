207473be2292d98c3076e52e333de1e82e047180
Bills <Brian.Bills@amr.net>
2019-07-09 09:27:10 -0700

    formatted file with shell-format

--- centos7-backout-kickstart-host.sh
+++ centos7-backout-kickstart-host.sh
@@ -1,6 +1,6 @@
 ################################################################################################
 # This script reverses changes made to the CentOS kickstart environment for a new CentOS host  #
-# build. It backs out the entry in dhcpd.conf of the dynamically static ip assignment for     #
+# build. It backs out the entry in dhcpd.conf of the dynamically static ip assignment for      #
 # the host being backed out. It removes the A record and the PTR, (pointer), record of the     #
 # host being backed out, and increments the serial numbers. It validates the accuracy and      #
 # neatness of the configuration changes. It backs up the new configuration changes. It ensures #
@@ -20,27 +20,27 @@ ZONES_PATH="/var/named"
 DATE=$(date +%Y%m%d)
 # we're looking line containing this comment
 NEEDLE="Serial"
-for ZONE in $(ls -1 $ZONES_PATH) ; do
-    curr=$(/bin/grep -e "${NEEDLE}$" $ZONES_PATH/${ZONE} | /bin/sed -n "s/^\s*\([0-9]*\)\s*;\s*${NEEDLE}\s*/\1/p")
-    # replace if current date is shorter (possibly using different format)
-    if [ ${#curr} -lt ${#DATE} ]; then
-      serial="${DATE}00"
+for ZONE in $(ls -1 $ZONES_PATH); do
+  curr=$(/bin/grep -e "${NEEDLE}$" $ZONES_PATH/${ZONE} | /bin/sed -n "s/^\s*\([0-9]*\)\s*;\s*${NEEDLE}\s*/\1/p")
+  # replace if current date is shorter (possibly using different format)
+  if [ ${#curr} -lt ${#DATE} ]; then
+    serial="${DATE}00"
+  else
+    prefix=${curr::-2}
+    if [ "$DATE" -eq "$prefix" ]; then # same day
+      num=${curr: -2}                       # last two digits from serial number
+      num=$((10#$num + 1))                  # force decimal representation, increment
+      serial="${DATE}$(printf '%02d' $num)" # format for 2 digits
     else
-      prefix=${curr::-2}
-      if [ "$DATE" -eq "$prefix" ]; then # same day
-        num=${curr: -2} # last two digits from serial number
-        num=$((10#$num + 1)) # force decimal representation, increment
-        serial="${DATE}$(printf '%02d' $num )" # format for 2 digits
-      else
-        serial="${DATE}00" # just update date
-      fi
+      serial="${DATE}00" # just update date
     fi
-    /bin/sed -i -e "s/^\(\s*\)[0-9]\{0,\}\(\s*;\s*${NEEDLE}\)$/\1${serial}\2/" ${ZONES_PATH}/${ZONE}
-    echo "${ZONE}: "
-    grep "; ${NEEDLE}$" $ZONES_PATH/${ZONE}
+  fi
+  /bin/sed -i -e "s/^\(\s*\)[0-9]\{0,\}\(\s*;\s*${NEEDLE}\)$/\1${serial}\2/" ${ZONES_PATH}/${ZONE}
+  echo "${ZONE}: "
+  grep "; ${NEEDLE}$" $ZONES_PATH/${ZONE}
 done
 systemctl start named
-# What to backup. 
+# What to backup.
 backup_files="/var/named /etc/named* /etc/dhcp/dhcpd.conf"" "$dhcp
 
 # Where to backup to.
@@ -48,7 +48,7 @@ dest="/root/backup"
 
 if [ ! -d "$dest" ]; then
   # Control will enter here if $dest doesn't exist.
-sudo mkdir -p /root/backup
+  sudo mkdir -p /root/backup
 fi
 
 # Create archive filename.
