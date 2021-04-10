74f2cc35fae1651b0cb9d324793549cc9ddb295d
Brian Bills <15617031+brianbills21@users.noreply.github.com>
2021-04-10 04:14:00 -0700

    Delete change_jenkins_certs.sh

--- change_jenkins_certs.sh
+++
@@ -1,9 +0,0 @@
-#Script called from root's crontab to change letsencrypt certs every 90 days
-#Called like this: 0 5 */90 * * /usr/share/jenkins/change_jenkins_certs.sh
-systemctl stop jenkins
-mv /usr/share/jenkins/fullchain1.pem /usr/share/jenkins/fullchain1.pem_$(date +%m-%d-%Y)
-mv /usr/share/jenkins/privkey-rsa.pem /usr/share/jenkins/privkey-rsa.pem_$(date +%m-%d-%Y)
-cp /etc/letsencrypt/live/jenkins.usreliance.com/privkey.pem /usr/share/jenkins/
-openssl rsa -in /usr/share/jenkins/privkey.pem -out /usr/share/jenkins/privkey-rsa.pem
-cp /etc/letsencrypt/live/jenkins.usreliance.com/fullchain.pem /usr/share/jenkins/fullchain1.pem
-systemctl start jenkins
