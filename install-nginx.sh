8c761c3c63199c1e11b899fa34e5b8885828c48f
brianbills21 <brianbills21@yahoo.com>
2022-08-12 21:36:11 -0700

    updated install-nginx.sh

--- install-nginx.sh
+++ install-nginx.sh
@@ -2,16 +2,16 @@
 if [[ ! -z $(netstat -an | grep "0.0.0.0:80") ]]; then echo "listening on 80"; else
 
 #Install nginx
-sudo apt install -y nginx
+apt install -y nginx
 
 #Create a folder for mj12net.org
-sudo mkdir /etc/nginx/sites-available/mj12net.org
+mkdir /etc/nginx/sites-available/mj12net.org
 
 #Link mj12net.org to sites.enabled
-sudo ln -s /etc/nginx/sites-available/mj12net.org /etc/nginx/sites-enabled/
+ln -s /etc/nginx/sites-available/mj12net.org /etc/nginx/sites-enabled/
 
 #Make /var/www/mj12net.org/html/index.html your home page
-sudo cat << EOF | sed '/^http {$/ r /dev/stdin' /etc/nginx/nginx.conf
+cat << EOF | sed '/^http {$/ r /dev/stdin' /etc/nginx/nginx.conf
 
 server {
         server_name  $(hostname -I | cut -d' ' -f1);
@@ -21,12 +21,12 @@ server {
 EOF
 
 #Zero out index.html
-sudo >/var/www/mj12net.org/html/index.html
+>/var/www/mj12net.org/html/index.html
 
 #Add mj12net.org to your host file
 
-sudo echo "$(hostname -I | cut -d' ' -f1) mj12net.org" >> /etc/hosts
+ echo "$(hostname -I | cut -d' ' -f1) mj12net.org" >> /etc/hosts
 
 #Restart nginx
-sudo systemctl restart nginx
+systemctl restart nginx
 fi
