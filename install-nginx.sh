f0b6a5a85914a7f7c1929772ecc4f467b8e361b0
brianbills21 <brianbills21@yahoo.com>
2022-08-12 21:14:25 -0700

    Added install-nginx.sh

---
+++ install-nginx.sh
@@ -0,0 +1,32 @@
+#Test if a daemon is listening on port 80, if not install nginx
+if [[ ! -z $(netstat -an | grep "0.0.0.0:80") ]]; then echo "listening on 80"; else
+
+#Install nginx
+sudo apt install -y nginx
+
+#Create a folder for mj12net.org
+sudo mkdir /etc/nginx/sites-available/mj12net.org
+
+#Link mj12net.org to sites.enabled
+sudo ln -s /etc/nginx/sites-available/mj12net.org /etc/nginx/sites-enabled/
+
+#Make /var/www/mj12net.org/html/index.html your home page
+sudo cat << EOF | sed '/^http {$/ r /dev/stdin' /etc/nginx/nginx.conf
+
+server {
+        server_name  192.168.1.168;
+        listen       80;
+        root         /var/www/mj12net.org/html;
+        index        index.html;
+EOF
+
+#Zero out index.html
+sudo >/var/www/mj12net.org/html/index.html
+
+#Add mj12net.org to your host file
+
+sudo echo "$(hostname -I | cut -d' ' -f1) mj12net.org" >> /etc/hosts
+
+#Check status of nginx
+sudo systemctl status nginx
+fi
