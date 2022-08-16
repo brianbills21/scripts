effc2b39787875e0d4e6c29cc15554cdad957d5a
brianbills21 <brianbills21@yahoo.com>
2022-08-16 13:46:24 -0700

    Create create-docker-jenkins.sh

---
+++ create-docker-jenkins.sh
@@ -0,0 +1,32 @@
+sudo su -
+mkdir -p /opt/jenkins/{jenkins-data,junkins-docker-certs}
+cat <<EOF > /opt/jenkins/Dockerfile
+FROM jenkins/jenkins:2.346.3-jdk11
+USER root
+RUN apt-get update && apt-get install -y lsb-release
+RUN curl -fsSLo /usr/share/keyrings/docker-archive-keyring.asc \
+  https://download.docker.com/linux/debian/gpg
+RUN echo "deb [arch=$(dpkg --print-architecture) \
+  signed-by=/usr/share/keyrings/docker-archive-keyring.asc] \
+  https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
+RUN apt-get update && apt-get install -y docker-ce-cli
+USER jenkins
+RUN jenkins-plugin-cli --plugins "blueocean:1.25.6 docker-workflow:1.29"
+EOF
+cd /opt/jenkins
+docker run \
+  --name jenkins-blueocean \
+  --detach \
+  --network jenkins \
+  --env DOCKER_HOST=tcp://docker:2376 \
+  --env DOCKER_CERT_PATH=/certs/client \
+  --env DOCKER_TLS_VERIFY=1 \
+  --publish 8080:8080 \
+  --publish 50000:50000 \
+  --volume jenkins-data:/var/jenkins_home \
+  --volume jenkins-docker-certs:/certs/client:ro \
+  --volume "$HOME":/home \
+  --restart=on-failure \
+  --env JAVA_OPTS="-Dhudson.plugins.git.GitSCM.ALLOW_LOCAL_CHECKOUT=true" \
+  myjenkins-blueocean:2.346.3-1
